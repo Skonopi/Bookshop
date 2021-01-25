e483d1819c4fef0a7614efc44780ad19ff26aedb
kzkysu <maja.orlow@gmail.com>
2021-01-26 00:02:08 +0100

    Added client.js

---
+++ client.js
@@ -0,0 +1,12 @@
+
+var cartButtons;
+
+cartButtons.array.forEach(btn => {
+    button.addEventListener('click', function(e) {
+        console.log('button was clicked');
+      });
+});
+//const button = document.getElementById('myButton');
+button.addEventListener('click', function(e) {
+  console.log('button was clicked');
+});
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const ejs = require('ejs');
+const cookieParser = require('cookie-parser');
 const  db = require('./database');
 
 var app = express()
@@ -8,14 +9,18 @@ var app = express()
 app.set('views','./views');
 app.set('view engine','html');
 
+app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 
 app.engine('html',ejs.renderFile);
 
-
 app.get('/', async (req,res) => {
     try {
         console.log("GET");
+        if (!req.cookies.usertype){
+            res.cookie('usertype','anonim');
+            console.log("Added usertype cookie");
+        }
         var keyword = req.query.keyword;
         var books;
         if (keyword) {
@@ -25,7 +30,7 @@ app.get('/', async (req,res) => {
             books = await db.getAllProducts();
         }
         console.log(books);
-        res.render('index.ejs',{'books':books});
+        res.render('index.ejs',{'books':books,'keyword':keyword});
         
     } catch (error) {
         console.log("Error while reading database");
@@ -38,7 +43,7 @@ app.get('/', async (req,res) => {
 app.post('/', (req,res) => {
     console.log("POST");
     var search = req.body.searchbar;
-    console.log("Seraching for: " + req.body.searchbar);
+    console.log("Searching for: " + req.body.searchbar);
     if(search){
         res.redirect('/?keyword='+search);
     }
