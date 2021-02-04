450362e743c9896dc703f12ad96ffae9e62221e7
Anna Pacanowska <306412@uwr.edu.pl>
2021-02-04 20:19:17 +0100

    Works now?

--- database.js
+++ database.js
@@ -313,12 +313,21 @@ class ShopRepository {
   }
 }
 
-var pool = new pg.Pool({
-  host: 'localhost',
-  database: 'shop',
-  user: 'app',
-  password: 'password'
-})
+// var pool = new pg.Pool({
+//   host: 'localhost',
+//   database: 'shop',
+//   user: 'app',
+//   password: 'password'
+// })
+
+const pool = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pool.connect();
 
 repo = new ShopRepository(pool);
 
